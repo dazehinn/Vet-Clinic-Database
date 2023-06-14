acc34e83892c954c3f1118f1054dfcc325b8a944
dazehinn <dazehinn@yahoo.fr>
2023-06-14 20:01:48 +0100

    Update date of birth for Pikachu

--- data.sql
+++ data.sql
@@ -5,5 +5,5 @@ INSERT INTO animals
   VALUES 
     ('Agumon', '2020-02-03', 10.23, true, 0),
     ('Gabumon', '2018-11-15', 8, true, 2),
-    ('Pikachu','2021-01-01', 15.04, false, 1),
+    ('Pikachu','2021-01-07', 15.04, false, 1),
     ('Devimon', '2017-05-12', 11, true, 5);
