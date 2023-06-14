71a4fe1c72bbae855a49d906bb8767642be6d8f1
dazehinn <dazehinn@yahoo.fr>
2023-06-14 18:35:49 +0100

    Add database schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE DATABASE vet_clinic;
+
+CREATE table animals (
+  id int,
+  name text,
+  date_of_birth date,
+  escape_attempts int,
+  neutered boolean,
+  weight_kg real 
 );
