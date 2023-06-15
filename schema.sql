5479e485b6ecf62de531907f1aa4ae9f4a3b8cbc
dazehinn <dazehinn@yahoo.fr>
2023-06-15 23:32:46 +0100

    Define species and owners table and add contraints

--- schema.sql
+++ schema.sql
@@ -15,3 +15,30 @@ CREATE table animals (
 ALTER TABLE animals
 ADD species varchar(250);
 
+/* Owners table */
+DROP TABLE IF EXISTS owners;
+
+CREATE TABLE owners (
+  id int PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+  full_name varchar(250),
+  age int
+);
+
+/* Species table*/
+DROP TABLE IF EXISTS species;
+CREATE TABLE species (
+  id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name varchar(10)
+);
+
+/* Modify animals table */
+ALTER TABLE animals
+DROP COLUMN species,
+ADD species_id int,
+ADD owner_id int;
+
+ALTER TABLE animals
+ADD CONSTRAINT owners_constraint
+ FOREIGN KEY(owner_id) REFERENCES owners(id),
+ADD CONSTRAINT species_constraint
+ FOREIGN KEY (species_id) REFERENCES species(id);
